13707cf2f8492b7ef83da645afb7a870f1a08f71
domtronn <dgc336@gmail.com>
2023-09-27 14:06:16 +0100

    Add preact runtimes into the same projects

--- deno/main.tsx
+++ deno/main.tsx
@@ -1,8 +1,11 @@
+import { h as preactRuntime } from 'https://esm.sh/preact'
+import preactRender from 'https://esm.sh/preact-render-to-string'
+
 import JSX from "https://esm.sh/react/jsx-runtime";
-import { renderToString } from "https://esm.sh/react-dom/server";
+import { renderToString as reactRender } from "https://esm.sh/react-dom/server";
 
 // @ts-ignore
-const { jsx } = JSX
+const { jsx: reactRuntime } = JSX
 
 const ITER = +(Deno.env.get("ITER") || 40)
 
@@ -11,25 +14,32 @@ function fibonacci(n: number): number {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+function renderJSX ({ runtime: jsx }: { runtime: any }) {
+  jsx('body', {
+    children: [
+      jsx('h1', { key: 'h1', children: 'Hello, world'}),
+      jsx('h2', { key: 'h2', children: `Here are the next ${ITER} fibonacci numbers`}),
+      jsx('section', {
+        key: 'section',
+        children: Array(+ITER)
+          .fill('')
+          .map((_, i) => (
+            jsx('p', { key: i , children: `Fibonacci ${i}: ${fibonacci(i)}`})
+          ))
+      })
+    ]})
+}
+
 Deno.bench(`Fibonacci ${ITER}`, () => {
   fibonacci(ITER)
 })
 
-Deno.bench(`Render to string ${ITER}`, async () => {
+Deno.bench(`React Render to string ${ITER}`, async () => {
   // @ts-ignore
-  await renderToString([
-    jsx('body', {
-      children: [
-        jsx('h1', { key: 'h1', children: 'Hello, world'}),
-        jsx('h2', { key: 'h2', children: `Here are the next ${ITER} fibonacci numbers`}),
-        jsx('section', {
-          key: 'section',
-          children: Array(+ITER)
-            .fill('')
-            .map((_, i) => (
-              jsx('p', { key: i , children: `Fibonacci ${i}: ${fibonacci(i)}`})
-            ))
-        })
-      ]})
-  ])
+  await reactRender(renderJSX({ runtime: reactRuntime }))
+})
+
+Deno.bench(`Preact Render to string ${ITER}`, async () => {
+  // @ts-ignore
+  await preactRender(renderJSX({ runtime: preactRuntime }))
 })
--- node/main.mjs
+++ node/main.mjs
@@ -1,6 +1,10 @@
 import { run, bench} from 'mitata'
-import { renderToString } from 'react-dom/server'
-import { jsx } from 'react/jsx-runtime'
+
+
+import { renderToString as reactRender } from 'react-dom/server'
+import { jsx as reactRuntime } from 'react/jsx-runtime'
+import preactRender from 'preact-render-to-string'
+import { h as preactRuntime} from 'preact'
 
 const { ITER = 40 } = process.env
 
@@ -9,26 +13,32 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+function renderJSX ({ runtime: jsx }) {
+  jsx('body', {
+    children: [
+      jsx('h1', { key: 'h1', children: 'Hello, world'}),
+      jsx('h2', { key: 'h2', children: `Here are the next ${ITER} fibonacci numbers`}),
+      jsx('section', {
+        key: 'section',
+        children: Array(+ITER)
+          .fill()
+          .map((_, i) => (
+            jsx('p', { key: i , children: `Fibonacci ${i}: ${fibonacci(i)}`})
+          ))
+      })
+    ]})
+}
+
 bench(`Fibonacci ${ITER}`, () => {
   fibonacci(ITER)
 })
 
-bench(`Render to string ${ITER}`, async () => {
-  await renderToString([
-    jsx('body', {
-      children: [
-        jsx('h1', { key: 'h1', children: 'Hello, world'}),
-        jsx('h2', { key: 'h2', children: `Here are the next ${ITER} fibonacci numbers`}),
-        jsx('section', {
-          key: 'section',
-          children: Array(+ITER)
-            .fill()
-            .map((_, i) => (
-              jsx('p', { key: i , children: `Fibonacci ${i}: ${fibonacci(i)}`})
-            ))
-        })
-      ]})
-  ])
+bench(`React Render to string ${ITER}`, async () => {
+  await reactRender(renderJSX({ runtime: reactRuntime }))
+})
+
+bench(`Preact Render to string ${ITER}`, async () => {
+  await preactRender(renderJSX({ runtime: preactRuntime }))
 })
 
 await run()
